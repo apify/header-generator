f39ee6dd8b9cdaa991eeb7b1242bc25645fba655
Equidem <matsochor@seznam.cz>
2021-03-17 22:31:30 +0100

    Separated BayesianNetwork into a new repository

--- src/header-generator.js
+++ src/header-generator.js
@@ -1,40 +1,40 @@
-const { BayesianNetwork } = require("./bayesian-network.js");
-const  fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const BayesianNetwork = require('bayesian-network');
 
-const headerNetworkDefinitionPath = path.join(__dirname, "./headerNetworkDefinition.json");
-const inputNetworkDefinitionPath = path.join(__dirname, "./inputNetworkDefinition.json");
-const browserHelperFilePath = path.join(__dirname, "./browserHelperFile.json");
+const headerNetworkDefinitionPath = path.join(__dirname, './headerNetworkDefinition.json');
+const inputNetworkDefinitionPath = path.join(__dirname, './inputNetworkDefinition.json');
+const browserHelperFilePath = path.join(__dirname, './browserHelperFile.json');
 
-const httpVersionNodeName = "*HTTP_VERSION";
-const browserNodeName = "*BROWSER";
-const operatingSystemNodeName = "*OPERATING_SYSTEM";
-const deviceNodeName = "*DEVICE";
-const missingValueDatasetToken = "*MISSING_VALUE*";
+const httpVersionNodeName = '*HTTP_VERSION';
+const browserNodeName = '*BROWSER';
+const operatingSystemNodeName = '*OPERATING_SYSTEM';
+const deviceNodeName = '*DEVICE';
+const missingValueDatasetToken = '*MISSING_VALUE*';
 
 const http2SecFetchAttributes = {
-    "mode": "sec-fetch-mode",
-    "dest": "sec-fetch-dest",
-    "site": "sec-fetch-site",
-    "user": "sec-fetch-user"
-}
+    mode: 'sec-fetch-mode',
+    dest: 'sec-fetch-dest',
+    site: 'sec-fetch-site',
+    user: 'sec-fetch-user',
+};
 
 const http1SecFetchAttributes = {
-    "mode": "Sec-Fetch-Mode",
-    "dest": "Sec-Fetch-Dest",
-    "site": "Sec-Fetch-Site",
-    "user": "Sec-Fetch-User"
-}
+    mode: 'Sec-Fetch-Mode',
+    dest: 'Sec-Fetch-Dest',
+    site: 'Sec-Fetch-Site',
+    user: 'Sec-Fetch-User',
+};
 
 function _getRandomInteger(minimum, maximum) {
     return minimum + Math.floor(Math.random() * (maximum - minimum + 1));
 }
 
 function _shuffleArray(array) {
-    if(array.length > 1) {
-        for(let x = 0; x < 10; x++) {
-            const position1 = _getRandomInteger(0, array.length-1);
-            const position2 = _getRandomInteger(0, array.length-1);
+    if (array.length > 1) {
+        for (let x = 0; x < 10; x++) {
+            const position1 = _getRandomInteger(0, array.length - 1);
+            const position2 = _getRandomInteger(0, array.length - 1);
             const holder = array[position1];
             array[position1] = array[position2];
             array[position2] = holder;
@@ -49,17 +49,17 @@ function _browserVersionIsLesserOrEquals(browserVersionL, browserVersionR) {
 }
 
 function _prepareBrowserObject(browserString) {
-    const nameVersionSplit = browserString.split("/");
-    const versionSplit = nameVersionSplit[1].split(".");
-    let preparedVersion = [];
-    for(let versionPart of versionSplit) {
+    const nameVersionSplit = browserString.split('/');
+    const versionSplit = nameVersionSplit[1].split('.');
+    const preparedVersion = [];
+    for (const versionPart of versionSplit) {
         preparedVersion.push(parseInt(versionPart));
     }
 
     return {
-        "name": nameVersionSplit[0],
-        "version": preparedVersion,
-        "completeString": browserString
+        name: nameVersionSplit[0],
+        version: preparedVersion,
+        completeString: browserString,
     };
 }
 
@@ -91,12 +91,12 @@ class HeaderGenerator {
      */
     constructor(options = {}) {
         this.defaultOptions = options;
-        let uniqueBrowserStrings = JSON.parse(fs.readFileSync(browserHelperFilePath, {encoding:'utf8'}));
+        const uniqueBrowserStrings = JSON.parse(fs.readFileSync(browserHelperFilePath, { encoding: 'utf8' }));
         this.uniqueBrowsers = [];
-        for(const browserString of uniqueBrowserStrings) {
-            if(browserString == missingValueDatasetToken) {
+        for (const browserString of uniqueBrowserStrings) {
+            if (browserString == missingValueDatasetToken) {
                 this.uniqueBrowsers.push({
-                    "name": missingValueDatasetToken
+                    name: missingValueDatasetToken,
                 });
             } else {
                 this.uniqueBrowsers.push(_prepareBrowserObject(browserString));
@@ -110,38 +110,38 @@ class HeaderGenerator {
      * @param {HeaderGeneratorOptions} options - main options overrides.
      */
     getHeaders(options) {
-        let headerOptions = {...this.defaultOptions, ...options};
-        if(!headerOptions.locales) {
-            headerOptions.locales = ["en-US"];
+        const headerOptions = { ...this.defaultOptions, ...options };
+        if (!headerOptions.locales) {
+            headerOptions.locales = ['en-US'];
         }
-        if(!headerOptions.httpVersion) {
-            headerOptions.httpVersion = "2";
+        if (!headerOptions.httpVersion) {
+            headerOptions.httpVersion = '2';
         }
-        if(!headerOptions.browsers) {
+        if (!headerOptions.browsers) {
             headerOptions.browsers = [
-                { name: "chrome" },
-                { name: "firefox" },
-                { name: "safari" }
+                { name: 'chrome' },
+                { name: 'firefox' },
+                { name: 'safari' },
             ];
         }
-        if(!headerOptions.operatingSystems) {
+        if (!headerOptions.operatingSystems) {
             headerOptions.operatingSystems = [
-                "windows",
-                "macos",
-                "linux",
-                "android",
-                "ios"
+                'windows',
+                'macos',
+                'linux',
+                'android',
+                'ios',
             ];
         }
 
-        let possibleAttributeValues = {};
+        const possibleAttributeValues = {};
 
-        let browserOptions = [];
-        for(const browser of headerOptions.browsers) {
-            for(const browserOption of this.uniqueBrowsers) {
-                if(browser.name == browserOption.name) {
-                    if((!browser.minVersion || _browserVersionIsLesserOrEquals([ browser.minVersion ], browserOption.version)) &&
-                        (!browser.maxVersion || _browserVersionIsLesserOrEquals(browserOption.version, [ browser.maxVersion ]))) {
+        const browserOptions = [];
+        for (const browser of headerOptions.browsers) {
+            for (const browserOption of this.uniqueBrowsers) {
+                if (browser.name == browserOption.name) {
+                    if ((!browser.minVersion || _browserVersionIsLesserOrEquals([browser.minVersion], browserOption.version))
+                        && (!browser.maxVersion || _browserVersionIsLesserOrEquals(browserOption.version, [browser.maxVersion]))) {
                         browserOptions.push(browserOption.completeString);
                     }
                 }
@@ -152,51 +152,51 @@ class HeaderGenerator {
 
         possibleAttributeValues[operatingSystemNodeName] = headerOptions.operatingSystems;
 
-        if(headerOptions.devices) {
+        if (headerOptions.devices) {
             possibleAttributeValues[deviceNodeName] = headerOptions.devices;
         }
-        possibleAttributeValues[httpVersionNodeName] = headerOptions.httpVersion == "2" ? [ "_2.0_" ] : [ "_1.0_", "_1.1_" ];
+        possibleAttributeValues[httpVersionNodeName] = headerOptions.httpVersion == '2' ? ['_2.0_'] : ['_1.0_', '_1.1_'];
 
-        let inputSample = this.inputGeneratorNetwork.generateSampleWheneverPossible(possibleAttributeValues);
+        const inputSample = this.inputGeneratorNetwork.generateSampleWheneverPossible(possibleAttributeValues);
 
-        if(!inputSample) throw "No headers based on this input can be generated. Please relax or change some of the requirements you specified.";
+        if (!inputSample) throw 'No headers based on this input can be generated. Please relax or change some of the requirements you specified.';
 
-        let generatedSample = this.headerGeneratorNetwork.generateSample(inputSample);
+        const generatedSample = this.headerGeneratorNetwork.generateSample(inputSample);
 
         let secFetchAttributeNames = http2SecFetchAttributes;
-        let acceptLanguageFieldName = "accept-language";
-        if(headerOptions.httpVersion !== "2") {
-            acceptLanguageFieldName = "Accept-Language";
+        let acceptLanguageFieldName = 'accept-language';
+        if (headerOptions.httpVersion !== '2') {
+            acceptLanguageFieldName = 'Accept-Language';
             secFetchAttributeNames = http1SecFetchAttributes;
         }
 
         let highLevelLocales = [];
-        let regionalLocales = [];
-        for(const locale of headerOptions.locales) {
-            if(!locale.includes("-")) {
+        const regionalLocales = [];
+        for (const locale of headerOptions.locales) {
+            if (!locale.includes('-')) {
                 highLevelLocales.push();
             }
         }
 
-        for(const locale of headerOptions.locales) {
-            if(!highLevelLocales.includes(locale)) {
+        for (const locale of headerOptions.locales) {
+            if (!highLevelLocales.includes(locale)) {
                 let highLevelEquivalentPresent = false;
-                for(let highLevelLocale of highLevelLocales) {
-                    if(locale.includes(highLevelLocale)) {
+                for (const highLevelLocale of highLevelLocales) {
+                    if (locale.includes(highLevelLocale)) {
                         highLevelEquivalentPresent = true;
                         break;
                     }
                 }
-                if(!highLevelEquivalentPresent) highLevelLocales.push(locale);
+                if (!highLevelEquivalentPresent) highLevelLocales.push(locale);
             }
         }
 
         highLevelLocales = _shuffleArray(highLevelLocales);
         headerOptions.locales = _shuffleArray(headerOptions.locales);
-        let localesInAddingOrder = [];
-        for(const highLevelLocale of highLevelLocales) {
-            for(const locale of headerOptions.locales) {
-                if(locale.includes(highLevelLocale) && !highLevelLocales.includes(locale)) {
+        const localesInAddingOrder = [];
+        for (const highLevelLocale of highLevelLocales) {
+            for (const locale of headerOptions.locales) {
+                if (locale.includes(highLevelLocale) && !highLevelLocales.includes(locale)) {
                     localesInAddingOrder.push(locale);
                 }
             }
@@ -204,31 +204,30 @@ class HeaderGenerator {
         }
 
         let acceptLanguageFieldValue = localesInAddingOrder[0];
-        for(let x = 1; x < localesInAddingOrder.length; x++) {
-            acceptLanguageFieldValue += "," + localesInAddingOrder[x] + ";" + (1 - x * 0.1);
+        for (let x = 1; x < localesInAddingOrder.length; x++) {
+            acceptLanguageFieldValue += `,${localesInAddingOrder[x]};${1 - x * 0.1}`;
         }
 
         generatedSample[acceptLanguageFieldName] = acceptLanguageFieldValue;
 
-        let generatedBrowser = _prepareBrowserObject(generatedSample[browserNodeName]);
-        if(generatedBrowser.name == "chrome") {
-            if(generatedBrowser.version[0] >= 76) {
-                generatedSample[secFetchAttributeNames["site"]] = "same-site";
-                generatedSample[secFetchAttributeNames["mode"]] = "navigate";
-                generatedSample[secFetchAttributeNames["user"]] = "?1";
-                if(generatedBrowser.version[0] >= 80) {
-                    generatedSample[secFetchAttributeNames["dest"]] = "document";
+        const generatedBrowser = _prepareBrowserObject(generatedSample[browserNodeName]);
+        if (generatedBrowser.name == 'chrome') {
+            if (generatedBrowser.version[0] >= 76) {
+                generatedSample[secFetchAttributeNames.site] = 'same-site';
+                generatedSample[secFetchAttributeNames.mode] = 'navigate';
+                generatedSample[secFetchAttributeNames.user] = '?1';
+                if (generatedBrowser.version[0] >= 80) {
+                    generatedSample[secFetchAttributeNames.dest] = 'document';
                 }
             }
         }
 
-        for(const attribute in generatedSample) {
-            if(attribute.startsWith("*") || generatedSample[attribute] == missingValueDatasetToken) delete generatedSample[attribute];
+        for (const attribute in generatedSample) {
+            if (attribute.startsWith('*') || generatedSample[attribute] == missingValueDatasetToken) delete generatedSample[attribute];
         }
 
         return generatedSample;
     }
-
 }
 
 module.exports = HeaderGenerator;
