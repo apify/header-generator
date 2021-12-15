1dd650f6e448463cbbc91b0cc8f716e03418b4bb
Petr Patek <petrpatekml@gmail.com>
2021-12-15 15:23:54 +0100

    Added basic strategies.

---
+++ src/strategies.js
@@ -0,0 +1,68 @@
+const MODERN_DESKTOP = {
+    browserListQuery: 'last 5 versions',
+};
+
+const MODERN_MOBILE = {
+    ...MODERN_DESKTOP,
+    devices: ['mobile'],
+};
+
+exports.MODERN_DESKTOP = MODERN_DESKTOP;
+exports.MODERN_MOBILE = MODERN_MOBILE;
+
+exports.MODERN_LINUX = {
+    ...MODERN_DESKTOP,
+    operatingSystems: ['linux'],
+};
+
+exports.MODERN_LINUX_FIREFOX = {
+    browserListQuery: 'last 5 firefox versions',
+    operatingSystems: ['linux'],
+};
+
+exports.MODERN_LINUX_CHROME = {
+    browserListQuery: 'last 5 chrome versions',
+    operatingSystems: ['linux'],
+};
+
+exports.MODERN_WINDOWS = {
+    ...MODERN_DESKTOP,
+    operatingSystems: ['windows'],
+};
+
+exports.MODERN_WINDOWS_FIREFOX = {
+    browserListQuery: 'last 5 firefox versions',
+    operatingSystems: ['windows'],
+};
+
+exports.MODERN_WINDOWS_CHROME = {
+    browserListQuery: 'last 5 chrome versions',
+    operatingSystems: ['windows'],
+};
+
+exports.MODERN_MACOS = {
+    ...MODERN_DESKTOP,
+    operatingSystems: ['macos'],
+};
+
+exports.MODERN_MACOS_FIREFOX = {
+    browserListQuery: 'last 5 firefox versions',
+    operatingSystems: ['macos'],
+};
+
+exports.MODERN_MACOS_CHROME = {
+    browserListQuery: 'last 5 chrome versions',
+    operatingSystems: ['macos'],
+};
+
+exports.MODERN_IOS = {
+    ...MODERN_MOBILE,
+    operatingSystems: ['ios'],
+
+};
+
+exports.MODERN_ANDROID = {
+    ...MODERN_MOBILE,
+    operatingSystems: ['ios'],
+
+};
